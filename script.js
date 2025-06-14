a601264c968b9e37a7bd32591110007b06a9a821
Rahat-Hridoy <rahathridoyd2x@gmail.com>
2025-06-14 20:25:40 +0600

    update responsive navbar and scrollabel project card

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// menu toggler
+let bar = document.querySelectorAll("#bar");
+let menu = document.getElementById("menu");
+let isVisible = false;
 
+function toggle() {
+  bar[0].classList.toggle("rotate-45");
+  bar[0].classList.toggle("translate-y-[10px]");
+  bar[1].classList.toggle("opacity-0");
+  bar[2].classList.toggle("-rotate-45");
+  bar[2].classList.toggle("-translate-y-[8px]");
+
+  menu.classList.toggle("translate-x-full");
+  menu.classList.toggle("translate-x-0");
+
+  isVisible = !isVisible;
+}
